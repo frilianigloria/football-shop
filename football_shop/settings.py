a8c0e7448342087eebc8e9afc0f6630a0f09abd8
frilianigloria <frilianigloriaa@gmail.com>
2025-09-10 08:25:13 +0700

    fixed settings

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -32,8 +32,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://pbp.cs.ui.ac.id/friliani.gloria/footballshop"]
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "friliani-gloria-footballshop.pbp.cs.ui.ac.id"]
 
 # Application definition
 
